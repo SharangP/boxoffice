0ad21dd56868161b30144c6e450907a7e65015a0
SharangP <sharang.phadke@gmail.com>
2013-10-17 21:25:53 -0400

    added lock files to gitignore, added basic movie search function to RTApi

--- rottentomatoes/rtapi.py
+++ rottentomatoes/rtapi.py
@@ -3,10 +3,23 @@ import urllib2
 
 class RTApi:
 
-    __baseUrl__ = "http://api.rottentomatoes.com/api/public/v1.0{endpoint}.json?apikey={api-key}"
+    __baseUrl__ = "http://api.rottentomatoes.com/api/public/v1.0{endpoint}.json?apikey={apiKey}{params}"
 
     def __init__(self,apiKey):
         self.__ApiKey__ = apiKey
 
-    def MovieInfo(self,id):
-        print 'go get the movie info'
+    def MovieSearch(self,query,limit):
+        results = []
+        url = self.__baseUrl__.format(
+                endpoint = "movies",
+                apiKey = self.__ApiKey__,
+                params = "&q=" + query + "&page_limit=" + str(limit))
+        url = url.replace(' ','%20')
+        try:
+            req = urllib2.Request(url)
+            jsonResponse = urllib2.urlopen(req)
+            results = json.load(jsonResponse)["movies"]
+            return results
+        except urllib2.URLError, e:
+            print e.message
+            return results
