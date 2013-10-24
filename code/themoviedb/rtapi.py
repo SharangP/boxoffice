715341439e7dc8ec55b3ac2360ebc8d5d27b394e
Shivam Mevawala <shivmevawala@gmail.com>
2013-10-24 00:22:13 -0400

    added themoviedb folder

---
+++ code/themoviedb/rtapi.py
@@ -0,0 +1,31 @@
+import os
+import json
+import urllib2
+
+class RTApi:
+
+    __baseUrl__ = "http://api.rottentomatoes.com/api/public/v1.0/{endpoint}.json?apikey={apiKey}{params}"
+
+    def __init__(self):
+        cd = os.path.dirname(os.path.abspath(__file__))
+        fp = os.path.join(cd, 'api.keys')
+        with open(fp) as f:
+            self.__ApiKey__ = json.load(f)["key"]
+        if not self.__ApiKey__:
+            raise("RT Api Key Error")
+
+    def MovieSearch(self,query,limit):
+        results = []
+        url = self.__baseUrl__.format(
+                endpoint = "movies",
+                apiKey = self.__ApiKey__,
+                params = "&q=" + query + "&page_limit=" + str(limit))
+        url = url.replace(' ','%20')
+        try:
+            req = urllib2.Request(url)
+            jsonResponse = urllib2.urlopen(req)
+            results = json.load(jsonResponse)["movies"]
+            return results
+        except urllib2.URLError, e:
+            print e.message
+            return results
