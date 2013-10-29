bea5d18f243d38a741e78f58c981e90671fd55f3
SharangP <sharang.phadke@gmail.com>
2013-10-28 22:42:22 -0400

    added pseudocode for scraping, made copy of old db

---
+++ code/scrape.py
@@ -0,0 +1,16 @@
+# Script to scrape movie metadata for movies in test set
+
+# load sagat csvs
+#   test - print data
+# movies <- sagat
+# connect to db
+# for movie in movies:
+#   id = TMDBApi.MovieSearch(movie)[0]["id"]
+#   info = TMDBApi.MovieById(id)
+#   cast = TMDBApi.MovieCastById(id)
+#   db.AddMovie <- info
+#   db.AddGenre <- info
+#   for person in cast: (not everyone)
+#       db.AddPerson(person.["id"],person["name"])
+#       db.AddCastMember(id,person["id"])
+
