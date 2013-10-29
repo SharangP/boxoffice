e99e66b713e03d5f6dd2057c763bba268c9f2954
Shivam Mevawala <shivmevawala@gmail.com>
2013-10-28 22:58:52 -0400

    set up database file

--- code/db/db.py
+++ code/db/db.py
@@ -7,9 +7,9 @@ class Database:
     with self.conn:
       try:
         self.cur.execute('CREATE TABLE IF NOT EXISTS movies (movie_id INTEGER PRIMARY KEY, movie_title VARCHAR(64), release_date VARCHAR(32), production_id INTEGER, production_name VARCHAR(64));')
-        self.cur.execute('CREATE TABLE IF NOT EXISTS people (person_id INTEGER PRIMARY KEY, person_name VARCHAR(32), movie_ids VARCHAR(32), mean_rating REAL, std_rating REAL));')
-        self.cur.execute('CREATE TABLE IF NOT EXISTS genres (movie_id INTEGER, genre_id INTEGER, PRIMARY KEY(movie_id, genre_id), genre_name VARCHAR(64), FOREIGN KEY(movie_id) REFERENCES movies(movie_id));')
-        self.cur.execute('CREATE TABLE IF NOT EXISTS cast (movie_id INTEGER , person_id INTEGER, PRIMARY KEY(movie_id, person_id), FOREIGN KEY(movie_id) REFERENCES movies(movie_id),FOREIGN KEY(person_id) REFERENCES people(person_id));')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS people (person_id INTEGER PRIMARY KEY, person_name VARCHAR(32), movie_ids VARCHAR(32), mean_rating REAL, std_rating REAL);')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS genres (movie_id INTEGER, genre_id INTEGER, genre_name VARCHAR(64), PRIMARY KEY(movie_id, genre_id), FOREIGN KEY(movie_id) REFERENCES movies(movie_id));')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS cast (movie_id INTEGER , person_id INTEGER, order_num INTEGER, PRIMARY KEY(movie_id, person_id), FOREIGN KEY(movie_id) REFERENCES movies(movie_id),FOREIGN KEY(person_id) REFERENCES people(person_id));')
         # self.categories = ["advertising", "agriculture", "art", "automotive", "aviation", "banking", "business", "celebrity", "computer", "disasters", "drugs", "economics", "education", "energy", "entertainment", "fashion", "finance", "food", "games", "health", "hobbies", "humor", "intellectual property", "labor", "legal", "lgbt", "marriage", "military", "mobile devices", "news", "philosophy", "politics", "real estate", "reference", "science", "sexuality", "shopping", "social media", "sports", "technology", "travel", "weapons", "weather", "none"]
 
         self.cur.execute('SELECT * FROM categories;')
