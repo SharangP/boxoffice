d1a682482f73a10a1b300b65c0d7b5d7a776ddb8
SharangP <sharang.phadke@gmail.com>
2013-10-28 23:38:12 -0400

    added csv reading

--- code/scrape.py
+++ code/scrape.py
@@ -1,8 +1,22 @@
 # Script to scrape movie metadata for movies in test set
+import csv
+from db.db import Database
+from themoviedb.tmdb import TMDBApi
+
+sagat = ['../datas/sagat_good_titles.csv', '../datas/sagat_bad_titles.csv']
+
+D = Database('boxoffice.db')
+TMDB = TMDBApi()
 
 # load sagat csvs
-#   test - print data
-# movies <- sagat
+for file in sagat:
+    with open(file, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for movie in reader:
+            mid = TMDB.MovieSearch(movie)[0]['id']
+            print mid, movie
+
+
 # connect to db
 # for movie in movies:
 #   id = TMDBApi.MovieSearch(movie)[0]["id"]
