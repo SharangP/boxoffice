67c650e10179948c845f4e2a95c7932050958df1
SharangP <sharang.phadke@gmail.com>
2013-11-08 15:57:41 -0500

    added inserting movies into RottenMovie table

--- code/actors.py
+++ code/actors.py
@@ -18,9 +18,11 @@ for person in [x for x in people]: #person[0] = id, person[1] = name
             movies = credits['cast']
         else:
             movies = credits['crew']
-        for movie in movies[0:2]:
+        for movie in movies:
             print "Processing movie: " + movie['title']
             rottenMovie = RT.MovieSearch(movie['title'], 1)
-            #D.addRotten()
+            if(len(rottenMovie) > 0):
+                D.AddRotten(movie['id'], movie['title'], rottenMovie[0]['ratings']['audience_score'], rottenMovie[0]['id'], movie['release_date'])
+                #D.AddRottenCast()
     else:
         print person[1] + " doesnt have credits"
