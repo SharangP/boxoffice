1a626a6cf1a44cb2cc55807b6c0406c0a258d6fc
SharangP <sharang.phadke@gmail.com>
2013-11-08 15:35:52 -0500

    added structure to actors scraping script

--- code/actors.py
+++ code/actors.py
@@ -6,12 +6,21 @@ D = Database('boxoffice.db')
 TMDB = TMDBApi()
 RT = RTApi()
 
-actors = D.GetAllPersonIdsNames()
-for actor in [x for x in actors[0:100]]: #actor[0] = id, actor[1] = name
-    credits = TMDB.PersonCreditsById(actor[0])
+people = D.GetAllPersonIdsNames()
+for person in [x for x in people]: #person[0] = id, person[1] = name
+    print "Processing person: " + person[1]
+    credits = TMDB.PersonCreditsById(person[0])
+    movies = []
     if len(credits) > 0:
-        credits['cast'].sort(key=lambda x: x['release_date'])
-        credits['crew'].sort(key=lambda x: x['release_date'])
-        print len(credits['cast']), len(credits['crew'])
+        #credits['cast'].sort(key=lambda x: x['release_date'])
+        #credits['crew'].sort(key=lambda x: x['release_date'])
+        if(len(credits['cast']) > len(credits['crew'])):
+            movies = credits['cast']
+        else:
+            movies = credits['crew']
+        for movie in movies[0:2]:
+            print "Processing movie: " + movie['title']
+            rottenMovie = RT.MovieSearch(movie['title'], 1)
+            #D.addRotten()
     else:
-        print actor[1] + " doesnt have credits"
+        print person[1] + " doesnt have credits"
