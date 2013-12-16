fde5b1e431b5e80c8a317809bcc9fa7c148cc3cc
smevawala <shivmevawala@gmail.com>
2013-12-16 17:39:15 -0500

    changed regression to plotting

--- code/regression.py
+++ code/regression.py
@@ -8,10 +8,9 @@ from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
  
-# def metrics(ax1, ax2, name, truth, guess, plotParams):
-def metrics(name, truth, guess, plotParams):
-    print str(name) + " results:"
-
+# def metrics(name, truth, guess, plotParams):
+def metrics(ax1, ax2, name, truth, guess, plotParams):
+    # print str(name) + " results:"
     Mean = np.mean(np.absolute(truth-guess))
     Median = np.median(np.absolute(truth-guess))
     R2 = r2_score(truth, guess)
@@ -26,12 +25,12 @@ def metrics(name, truth, guess, plotParams):
     X = [x[0] for x in sortedError]
     Y = [x[1] for x in sortedError]
 
-    # ax1.plot(X, Y, plotParams)
-    # ax1.plot([0,100],[0,0], 'k')
-    # ax1.plot([0,100],[std,std], 'k')
-    # ax1.plot([0,100],[-std,-std], 'k')
+    ax1.plot(X, Y, plotParams)
+    ax1.plot([0,100],[0,0], 'k')
+    ax1.plot([0,100],[std,std], 'k')
+    ax1.plot([0,100],[-std,-std], 'k')
 
-    # ax1.axis([0,100,-100,100])
+    ax1.axis([0,100,-100,100])
 
     # ax2.hist(guess)
     
@@ -52,7 +51,7 @@ algorithms = [
     RandomForestRegressor(n_estimators=200)
     ]
 
-niter = 500
+niter = 5
 means = np.zeros([len(algorithms),niter])
 medians = np.zeros([len(algorithms),niter])
 r2s = np.zeros([len(algorithms),niter])
@@ -98,32 +97,32 @@ for n in xrange(niter):
     ###############################################################################
     # Compute metrics
 
-    # plt.hold(True)
-    # p1 = plt.figure()
-    # p2 = plt.figure()
-    # p3 = plt.figure()
-    # p4 = plt.figure()
-    # p5 = plt.figure()
+    plt.hold(True)
+    p1 = plt.figure()
+    p2 = plt.figure()
+    p3 = plt.figure()
+    p4 = plt.figure()
+    p5 = plt.figure()
 
-    # ax1 = p1.add_subplot(111)
-    # ax2 = p2.add_subplot(111)
-    # ax3 = p3.add_subplot(111)
-    # ax4 = p4.add_subplot(111)
+    ax1 = p1.add_subplot(111)
+    ax2 = p2.add_subplot(111)
+    ax3 = p3.add_subplot(111)
+    ax4 = p4.add_subplot(111)
 
-    # ax51 = p5.add_subplot(221)
-    # ax52 = p5.add_subplot(222)
-    # ax53 = p5.add_subplot(223)
-    # ax54 = p5.add_subplot(224)
-# 
-#     means[0,n], medians[0,n] = metrics(ax1, ax51, "SVR", S_test, guess[0], 'ro')
-#     means[1,n], medians[1,n] = metrics(ax2, ax52, "Boosted Decision Tree", S_test, guess[1], 'bo')
-#     means[2,n], medians[2,n] = metrics(ax3, ax53, "Gradient Boosting Regression", S_test, guess[2], 'go')
-#     means[3,n], medians[3,n] = metrics(ax4, ax54, "Random Forest Regression", S_test, guess[3], 'ko')
-# 
-    means[0,n], medians[0,n], r2s[0,n] = metrics("SVR", S_test, guess[0], 'ro')
-    means[1,n], medians[1,n], r2s[1,n] = metrics("Boosted Decision Tree", S_test, guess[1], 'bo')
-    means[2,n], medians[2,n], r2s[2,n] = metrics("Gradient Boosting Regression", S_test, guess[2], 'go')
-    means[3,n], medians[3,n], r2s[3,n] = metrics("Random Forest Regression", S_test, guess[3], 'ko')
+    ax51 = p5.add_subplot(221)
+    ax52 = p5.add_subplot(222)
+    ax53 = p5.add_subplot(223)
+    ax54 = p5.add_subplot(224)
+
+    means[0,n], medians[0,n], r2s[0,n] = metrics(ax1, ax51, "SVR", S_test, guess[0], 'ro')
+    means[1,n], medians[1,n], r2s[1,n] = metrics(ax2, ax52, "Boosted Decision Tree", S_test, guess[1], 'bo')
+    means[2,n], medians[2,n], r2s[2,n] = metrics(ax3, ax53, "Gradient Boosting Regression", S_test, guess[2], 'go')
+    means[3,n], medians[3,n], r2s[3,n] = metrics(ax4, ax54, "Random Forest Regression", S_test, guess[3], 'ko')
+
+    # means[0,n], medians[0,n], r2s[0,n] = metrics("SVR", S_test, guess[0], 'ro')
+    # means[1,n], medians[1,n], r2s[1,n] = metrics("Boosted Decision Tree", S_test, guess[1], 'bo')
+    # means[2,n], medians[2,n], r2s[2,n] = metrics("Gradient Boosting Regression", S_test, guess[2], 'go')
+    # means[3,n], medians[3,n], r2s[3,n] = metrics("Random Forest Regression", S_test, guess[3], 'ko')
 
     # ax51.hist(S_test)
     # ax42.hist(boostedTreeGuess)
@@ -133,4 +132,4 @@ print np.mean(means,1)
 print np.mean(medians,1)
 print np.mean(r2s,1)
 
-# plt.show()
+plt.show()
