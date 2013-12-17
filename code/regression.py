1498458e3184ead0bb906eb04d03b4fe4aae6930
SharangP <sharang.phadke@gmail.com>
2013-12-16 22:08:46 -0500

    added figures and made figure generation modular in regression script

--- code/regression.py
+++ code/regression.py
@@ -7,10 +7,10 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
- 
+
 # def metrics(name, truth, guess, plotParams):
 def metrics(ax1, ax2, name, truth, guess, plotParams):
-    # print str(name) + " results:"
+    print str(name) + " results:"
     Mean = np.mean(np.absolute(truth-guess))
     Median = np.median(np.absolute(truth-guess))
     R2 = r2_score(truth, guess)
@@ -31,17 +31,21 @@ def metrics(ax1, ax2, name, truth, guess, plotParams):
     ax1.plot([0,100],[-std,-std], 'k')
 
     ax1.axis([0,100,-100,100])
+    ax1.set_title(str(name) + " Sample Run Error")
+    ax1.set_xlabel("Ground Truth Audience Film Rating")
+    ax1.set_ylabel("Estimated Audience Film Rating")
 
-    # ax2.hist(guess)
-    
-    return Mean, Median, R2
+    ax2.hist(guess)
+    ax2.axis([0,100,0,100])
+    ax2.set_title(str(name))
+    # ax2.set_xlabel("Estimated Audience Film Rating")
+    return Mean, Median, R2, sortedError
 
 D = Database('boxoffice.db')
 
 ###############################################################################
 # Generate features from db, split into training and testing sets
 F1 = np.array(D.GetAllFeatures())
-
 F1 = np.array([x for x in F1 if all(x[5:] != -1)])      # remove examples with missing people
 
 algorithms = [
@@ -50,11 +54,38 @@ algorithms = [
     GradientBoostingRegressor(loss='huber', n_estimators=200),
     RandomForestRegressor(n_estimators=200)
     ]
+names=["SVR", "Boosted Decision Tree", "Gradient Boosting Regression", "Random Forest Regression"]
 
-niter = 5
+niter = 1
+train_ind = int(F1.shape[0]*4/5)
+test_ind = F1.shape[0] - train_ind
+
 means = np.zeros([len(algorithms),niter])
 medians = np.zeros([len(algorithms),niter])
 r2s = np.zeros([len(algorithms),niter])
+errors = [[],[],[],[]]
+
+plt.hold(True)
+p1 = plt.figure()
+p2 = plt.figure()
+p3 = plt.figure()
+p4 = plt.figure()
+p5 = plt.figure()
+p6 = plt.figure()
+
+ax1 = p1.add_subplot(111)
+ax2 = p2.add_subplot(111)
+ax3 = p3.add_subplot(111)
+ax4 = p4.add_subplot(111)
+
+ax51 = p5.add_subplot(221)
+ax52 = p5.add_subplot(222)
+ax53 = p5.add_subplot(223)
+ax54 = p5.add_subplot(224)
+p5.suptitle("Estimated Audience Film Rating Histograms")
+
+ax6 = p6.add_subplot(111)
+
 
 for n in xrange(niter):
     print "Begin iteration " + str(n) + "\n"
@@ -72,18 +103,10 @@ for n in xrange(niter):
     F = np.append(F, StdCast, 1)
     # F=F1[:,5:]
 
-    train_ind = int(F.shape[0]*9/10)
     F_train = F[:train_ind,:]
     S_train = S[:train_ind]
     F_test = F[train_ind:,]
     S_test = S[train_ind:]
-
-    ###############################################################################
-    # PCA
-
-    # pca = PCA()
-    # F_train = pca.fit_transform(F_train)
-    # F_test = pca.transform(F_test)
 
     ###############################################################################
     # Fit regression models
@@ -97,39 +120,60 @@ for n in xrange(niter):
     ###############################################################################
     # Compute metrics
 
-    plt.hold(True)
-    p1 = plt.figure()
-    p2 = plt.figure()
-    p3 = plt.figure()
-    p4 = plt.figure()
-    p5 = plt.figure()
+    means[0,n], medians[0,n], r2s[0,n], tmpE0 = metrics(ax1, ax51, names[0], S_test, guess[0], 'ro')
+    means[1,n], medians[1,n], r2s[1,n], tmpE1 = metrics(ax2, ax52, names[1], S_test, guess[1], 'bo')
+    means[2,n], medians[2,n], r2s[2,n], tmpE2 = metrics(ax3, ax53, names[2], S_test, guess[2], 'go')
+    means[3,n], medians[3,n], r2s[3,n], tmpE3 = metrics(ax4, ax54, names[3], S_test, guess[3], 'ko')
 
-    ax1 = p1.add_subplot(111)
-    ax2 = p2.add_subplot(111)
-    ax3 = p3.add_subplot(111)
-    ax4 = p4.add_subplot(111)
-
-    ax51 = p5.add_subplot(221)
-    ax52 = p5.add_subplot(222)
-    ax53 = p5.add_subplot(223)
-    ax54 = p5.add_subplot(224)
-
-    means[0,n], medians[0,n], r2s[0,n] = metrics(ax1, ax51, "SVR", S_test, guess[0], 'ro')
-    means[1,n], medians[1,n], r2s[1,n] = metrics(ax2, ax52, "Boosted Decision Tree", S_test, guess[1], 'bo')
-    means[2,n], medians[2,n], r2s[2,n] = metrics(ax3, ax53, "Gradient Boosting Regression", S_test, guess[2], 'go')
-    means[3,n], medians[3,n], r2s[3,n] = metrics(ax4, ax54, "Random Forest Regression", S_test, guess[3], 'ko')
+    errors[0].extend(tmpE0)
+    errors[1].extend(tmpE1)
+    errors[2].extend(tmpE2)
+    errors[3].extend(tmpE3)
 
     # means[0,n], medians[0,n], r2s[0,n] = metrics("SVR", S_test, guess[0], 'ro')
     # means[1,n], medians[1,n], r2s[1,n] = metrics("Boosted Decision Tree", S_test, guess[1], 'bo')
     # means[2,n], medians[2,n], r2s[2,n] = metrics("Gradient Boosting Regression", S_test, guess[2], 'go')
     # means[3,n], medians[3,n], r2s[3,n] = metrics("Random Forest Regression", S_test, guess[3], 'ko')
 
-    # ax51.hist(S_test)
-    # ax42.hist(boostedTreeGuess)
-    # ax43.hist(S_test-boostedTreeGuess)
+    ax6.hist(S_test)
+    ax6.set_title("Ground Truth Audience Rating Histogram")
+    ax6.axis([0,100,0,100])
 
 print np.mean(means,1)
 print np.mean(medians,1)
 print np.mean(r2s,1)
 
+p7 = plt.figure()
+p7.suptitle(" Sample Run Error")
+grpSize = 2*niter
+
+for i in xrange(len(algorithms)):
+    X = []
+    Y = []
+    Z = []
+    fullSortedErrors = sorted(errors[i], key=lambda pair: pair[0])
+    splnum=220+i
+    ax7 = p7.add_subplot(splnum)
+    for n in xrange(int(math.ceil(len(fullSortedErrors)/grpSize))):
+        X.append( np.mean([x[0] for x in fullSortedErrors[n*grpSize:(n+1)*grpSize]]))
+        Y.append( np.mean([x[1] for x in fullSortedErrors[n*grpSize:(n+1)*grpSize]]))
+        Z.append( np.std([x[1] for x in fullSortedErrors[n*grpSize:(n+1)*grpSize]]))
+    # X = [x[0] for x in fullSortedErrors]
+    # Y = [x[1] for x in fullSortedErrors]
+    std = np.std(Y)
+
+    ax7.plot(X, Y, 'b')
+    ax7.plot(X, [y-z for y, z in zip(Y, Z)], 'r')
+    ax7.plot(X, [y+z for y, z in zip(Y, Z)], 'r')
+
+    ax7.plot([0,100],[0,0], 'k')
+    ax7.plot([0,100],[std,std], 'k')
+    ax7.plot([0,100],[-std,-std], 'k')
+
+    ax7.axis([0,100,-100,100])
+    ax7.set_title(str(names[i]))
+    ax7.set_xlabel("Ground Truth Audience Film Rating")
+    ax7.set_ylabel("Estimated Audience Film Rating")
+
 plt.show()
+
